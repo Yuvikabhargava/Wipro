e5f907cfd0890d69c786bf515e3a9b7df7eea219
Yuvikabhargava <bhargavayuvika@gmail.com>
2025-01-29 10:49:08 +0530

    yuvika

---
+++ day2.cs
@@ -0,0 +1,27 @@
+﻿namespace wiprosampleproject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee emp = new Employee();
+            emp.emp_Id = 111;
+            emp.name = "Test";
+
+            Console.WriteLine($"Employee ID: {emp.emp_Id]");
+            Console.₩riteLine(S"Employee Name:{emp.name)");
+
+
+            Console.WriteLine("Enter the Employee Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the Employee Name:");
+            string name = Console.ReadLine();
+
+
+            Employee emp2 = new Employee(id, name);
+        }
+    }
+}
