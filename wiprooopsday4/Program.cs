e901f01524fea9261a47e8c1147ba03873ea3112
Yuvikabhargava <86181668+Yuvikabhargava@users.noreply.github.com>
2025-01-30 16:01:27 +0530

    Add files via upload

---
+++ wiprooopsday4/Program.cs
@@ -0,0 +1,21 @@
+﻿using wiproday4;
+
+namespace wiproday4
+{ 
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Student stu = new Student();
+            stu.Stuid = 110;
+            stu.Name = "Siya";
+            stu.Age = 4;
+            Console.WriteLine(stu.Stuid);
+            Console.WriteLine(stu.Name);
+            Console.WriteLine(stu.Age);
+            //stu.SId = 111;//set accessor will invoke
+            //Console.WriteLine(stu.SId);//get accessot will invoke
+        }
+    }
+}
